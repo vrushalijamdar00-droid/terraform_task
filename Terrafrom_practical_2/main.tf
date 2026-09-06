79dc0d42e69b71721cb5d04bda6a1d5a243a6ea9
Your Name <your-email@example.com>
2026-09-06 10:44:27 +0530

    new files

---
+++ Terrafrom_practical_2/main.tf
@@ -0,0 +1,5 @@
+resource "aws_instance" "test"{
+    ami                = "var.ami_id "
+    instance_type      = "var.instance_type"
+   
+}
---
+++ Terrafrom_practical_2/variable.tf
@@ -0,0 +1,10 @@
+variable "ami_id"{
+    description = "AMI ID for EC2 instance" 
+    type = string
+
+}
+variable "instance_type"{
+    description = "EC2 instance type" 
+    type = string
+
+}
