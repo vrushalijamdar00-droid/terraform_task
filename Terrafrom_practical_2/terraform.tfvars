5d9f1becf384243b14a279a12d4d048a1c86fea5
Your Name <your-email@example.com>
2026-09-06 11:04:09 +0530

    first commit

--- Terrafrom_practical_2/terraform.tfvars
+++ Terrafrom_practical_2/terraform.tfvars
@@ -1,3 +1,3 @@
-ami_id = "ami-05bfa4a7765f38076"
+ami_id          = "ami-05bfa4a7765f38076"
 
 instance_type = "t2.small"
--- Terrafrom_practical_2/variable.tf
+++ Terrafrom_practical_2/variable.tf
@@ -1,10 +1,10 @@
 variable "ami_id"{
     description = "AMI ID for EC2 instance" 
-    type = string
+    type        = string
 
 }
 variable "instance_type"{
     description = "EC2 instance type" 
-    type = string
+    type        = string
 
 }
